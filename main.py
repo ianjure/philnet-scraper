3e0f40e632b080123235ec7058e713c53ea402cc
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2025-05-25 08:36:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def fetch_multiple_urls(urls: List[str]) -> List[Dict[str, str]]:
 async def main():
     JSON_URL = "http://data.phishtank.com/data/online-valid.json"
     max_retries = 3
-    retry_delay = 120
+    retry_delay = 300
 
     # Fetch data from PhishTank
     for attempt in range(max_retries):
